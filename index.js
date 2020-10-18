07afc2c2c3cb0b0553d9cd1814ae6a28a5065dba
Lee Seong Won <syanggu@naver.com>
2020-10-18 21:29:20 +0900

    Mongo DB Connected

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose
+  .connect(
+    'mongodb+srv://<username>:<password>@wonshop.fewqi.mongodb.net/wonshop?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    },
+  )
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello world!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
